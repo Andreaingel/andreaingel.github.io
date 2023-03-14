40a64bb40f7f3a5d0cb79bfc361f4bb896616e2b
Andreaingel <97047431+Andreaingel@users.noreply.github.com>
2023-03-14 11:24:14 -0300

    Initial commit

---
+++ function.js
@@ -0,0 +1,14 @@
+
+document.getElementById('boton').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Mi Curriculum Vitae";
+}
+console.log("Hola mundo");
+
+let numero1 = 3;
+let numero2 = 4;
+
+let resultado = numero1+numero2;
+
+console.log("La suma de numero1 y numero2 es"+ resultado);
+
